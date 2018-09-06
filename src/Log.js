a8db97f9298b63a569d267dc08a45141195d439d
Chase McManning <cmcmanning@gmail.com>
2018-09-05 21:26:11 -0400

    Add Logger built on debug.js to improve component logging

---
+++ src/Log.js
@@ -0,0 +1,25 @@
+
+import debug from 'debug';
+
+/**
+ * Wrapper for debug.js to handle different log message levels
+ */
+class Logger {
+    constructor(namespace) {
+        this.debug = debug('drawover:' + namespace);
+    }
+
+    info() {
+        this.debug('%c info: ', 'background-color: #66c2cd; color: #ffffff', ...arguments);
+    }
+
+    warn() {
+        this.debug('%c warning: ', 'background-color: #dbaa79; color: #ffffff', ...arguments);
+    }
+
+    error() {
+        this.debug('%c error: ', 'background-color: #e88388; color: #ffffff', ...arguments);
+    }
+}
+
+export default Logger;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ import App from './App';
 import Draw from './Draw';
 import Transform from './Transform';
 
+// Activate the debug library on non-production using a key in localStorage
+if (process.env.NODE_ENV !== 'production') {
+    localStorage.setItem('debug', 'drawover:*');
+}
+
 // ReactDOM.render(
 //     <Transform>
 //         <Draw />
